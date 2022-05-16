6a0e9c4bcaf009a93bf4cd5647e6f89d9509c700
global-Toba-dean <global-tobaogundimu@gmail.com>
2022-05-16 13:21:08 +0100

    done with most of the build

---
+++ components/Feed.js
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+
+import { Input } from "./Input";
+import { Post } from "./Post";
+
+import { useRecoilState } from "recoil";
+import { handlePostState, useSSRPostsState } from "../atoms/postAtom";
+
+const Feed = ({ posts }) => {
+
+  const [realtimePosts, setRealtimePosts] = useState([]);
+  const [handlePost, setHandlePost] = useRecoilState(handlePostState);
+  const [useSSRPosts, setUseSSRPosts] = useRecoilState(useSSRPostsState);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/posts", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
+
+      const responseData = await response.json();
+      setRealtimePosts(responseData);
+      setHandlePost(false);
+      setUseSSRPosts(false);
+    };
+
+    fetchPosts();
+  }, [handlePost]);
+
+  return (
+    <div className="space-y-6 pb-24 max-w-xl">
+      <Input />
+      {
+        !useSSRPosts ? (
+          realtimePosts.map(post => (
+            <Post key={post.id} post={post} />
+          )) 
+        ) : (
+          posts.map(post => (
+            <Post key={post.id} post={post} />
+          ))
+        )
+      }
+    </div>
+  )
+}
+
+export default Feed
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,36 @@
 import Head from 'next/head'
-import { signOut } from 'next-auth/react'
+import { getSession, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 import { Header } from '../components/Header'
+import { Sidebar } from '../components/Sidebar'
+import Feed from '../components/Feed';
+import Modal from '../components/Modal';
+import { Widget } from '../components/Widget';
 
-export default function Home() {
+
+import { useRecoilState } from "recoil";
+import { modalState, modalTypeState } from "../atoms/modalAtom";
+
+import { AnimatePresence } from "framer-motion";
+
+import { connectToDatabase } from "../utils/mongodb";
+
+
+export default function Home({ posts, articles }) {
+
+  const [modalOpen, setModalOpen] = useRecoilState(modalState);
+  const [modalType, setModalType] = useRecoilState(modalTypeState);
+  const router = useRouter();
+
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      // The user is not authenticated, handle it here.
+      router.push("/landin");
+    },
+  });
+
   return (
     <div className="bg-[#F3F2EF] dark:bg-black dark:text-white h-screen overflow-y-scroll md:space-y-6">
       <Head>
@@ -13,9 +40,60 @@ export default function Home() {
 
       <Header />
 
-      <button onClick={signOut}>
-        Sign Out
-      </button>
+      <main className="flex justify-center gap-x-5 px-4 sm:px-12">
+        <div className="flex flex-col md:flex-row gap-5">
+          <Sidebar />
+          <Feed posts={posts} />
+        </div>
+
+        <Widget articles={articles} />
+        <AnimatePresence>
+          {modalOpen && (
+            <Modal handleClose={() => setModalOpen(false)} type={modalType} />
+          )}
+        </AnimatePresence>
+      </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  if(!session) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/landin",
+      }
+    }
+  }
+
+   // Get posts on SSR
+   const { db } = await connectToDatabase();
+   const posts = await db
+     .collection("posts")
+     .find()
+     .sort({ timestamp: -1 })
+     .toArray();
+ 
+   // Get Google News API
+   const results = await fetch(
+     `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`
+   ).then((res) => res.json());
+
+  return {
+    props: {
+      session,
+      posts: posts.map(post => ({
+        _id: post._id.toString(),
+        input: post.input,
+        photoUrl: post.photoUrl,
+        username: post.username,
+        email: post.email,
+        userImg: post.userImg,
+        createdAt: post.createdAt,
+      })),
+      articles: results.articles,
+    }
+  }
 }
